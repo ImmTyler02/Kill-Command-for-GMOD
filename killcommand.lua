4f7f19601cd1fab50c3e6ec6077e1c0b6b7296f0
Tomm <tylerscottjones20@gmail.com>
2025-02-08 22:46:20 -0500

    Update killcommand.lua

--- killcommand.lua
+++ killcommand.lua
@@ -1,13 +1,11 @@
-function testing(ply, text, team)
-
-	if text == "!kill" then
-
-		ply:Kill()
-	end
-
+local function KillCommand(ply, text, teamChat)
+    if string.lower(text) == "!kill" then
+        ply:Kill()
+        return ""
+    end
 end
 
-hook.Add("PlayerSay", "UniquieName", testing)
+hook.Add("PlayerSay", "KillCommand", KillCommand)
 
-	
-
+---Updated on 2/8/2025 to change the format to be local to reduce conflics that may occur and added string.lower so that the only way thsi code runs
+---is if it is correctly imputed as in "!kill" and would not accept something like "!KILL"
